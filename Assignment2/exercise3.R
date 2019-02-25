7e42e6f158d960e2b87e43caa2f2bf451b326802
Kaan Kara <kaan_k193@hotmail.com>
2019-02-25 17:22:03 +0100

    Start exercise three

---
+++ Assignment2/exercise3.R
@@ -0,0 +1,21 @@
+klmData = scan("./data/klm.txt")
+
+par(mfrow=c(1,3))
+# This doen't look like it is from normal distribution?
+hist(klmData, freq=FALSE)
+qqnorm(klmData)
+boxplot(klmData)
+
+# H_0 median duration is <= 31 days
+testMedian = 31
+
+klmMedian = median(klmData)
+
+# We expect median to divide the data set into two equal parts
+# so that when a random sample is chosen, the probability of it
+# being smaller or greater than the median should be equal to
+# tossing a coin
+
+sumOut = sum(klmData <= testMedian) # Get values smaller than the test value
+
+binom.test(sumOut, length(klmData), p=0.5)
