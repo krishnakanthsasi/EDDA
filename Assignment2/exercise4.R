43f60088f100b951af54d37fd756261cc82f1dd0
Kaan Kara <kaan_k193@hotmail.com>
2019-03-01 01:07:21 +0100

    Finish exercise 4 without comments

--- Assignment2/exercise4.R
+++ Assignment2/exercise4.R
@@ -3,8 +3,8 @@ sqrtClouds = sqrt(clouds)
 sqrtSqrtClouds = sqrt(sqrtClouds)
 
 par(mfrow=c(1,2))
-hist(clouds$seeded - clouds$unseeded, main = "Histogram of Cloud Differences")
-qqnorm(clouds$seeded - clouds$unseeded, main = "Normal Q-Q Plot Clouds Differences")
+hist(clouds$seeded - clouds$unseeded, main = "Histogram of Differences")
+qqnorm(clouds$seeded - clouds$unseeded, main = "Normal Q-Q Plot Differences")
 
 # T - Test
 
@@ -21,3 +21,39 @@ wilcox.test(clouds$seeded, clouds$unseeded)
 # Kolmogorov - Smirnov Test
 
 ks.test(clouds$seeded, clouds$unseeded)
+
+# Section 2
+
+par(mfrow=c(1,2))
+hist(sqrtClouds$seeded - sqrtClouds$unseeded, main = "Histogram of Sqrt Differences")
+qqnorm(sqrtClouds$seeded - sqrtClouds$unseeded, main = "Normal Q-Q Sqrt Differences")
+
+# T - Test
+
+t.test(sqrtClouds$seeded, sqrtClouds$unseeded)
+
+# Mann - Whitney Test
+
+wilcox.test(sqrtClouds$seeded, sqrtClouds$unseeded)
+
+# Kolmogorov - Smirnov Test
+
+ks.test(sqrtClouds$seeded, sqrtClouds$unseeded)
+
+# Section 3
+
+par(mfrow=c(1,2))
+hist(sqrtSqrtClouds$seeded - sqrtSqrtClouds$unseeded, main = "Histogram of SqrtSqrt Differences")
+qqnorm(sqrtSqrtClouds$seeded - sqrtSqrtClouds$unseeded, main = "Normal Q-Q SqrtSqrt Differences")
+
+# T - Test
+
+t.test(sqrtSqrtClouds$seeded, sqrtSqrtClouds$unseeded)
+
+# Mann - Whitney Test
+
+wilcox.test(sqrtSqrtClouds$seeded, sqrtSqrtClouds$unseeded)
+
+# Kolmogorov - Smirnov Test
+
+ks.test(sqrtSqrtClouds$seeded, sqrtSqrtClouds$unseeded)
